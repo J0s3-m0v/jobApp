9cf0e1d41400f7f3623ee9c3f30b6e3c52346df9
J0s3-m0v <j0s3.m0v@outlook.es>
2020-07-27 18:53:21 +0100

    routes backend

---
+++ backend/routes/job.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var JobController = require('../controllers/job');
+
+var router = express.Router();
+
+router.get('/home', JobController.home);
+
+router.post('/test', JobController.test);
+
+router.post('/saveJob', JobController.saveJob);
+
+router.get('/job/:id?', JobController.getJob);
+
+router.get('/jobs', JobController.getJobs);
+
+router.put('/job/:id', JobController.updateJob);
+
+router.delete('/job/:id', JobController.removeJob);
+
+module.exports = router;
