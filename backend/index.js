e2e2fd62f7aba089e1537ea99bf9f387d7d7587b
J0s3-m0v <j0s3.m0v@outlook.es>
2020-07-27 18:14:07 +0100

    Start backend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+
+var mongoose = require('mongoose');
+var app = require('./app');
+var port =3700;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/jobDB')
+.then(()=>{
+    console.log("The Database was connected successfully");
+    // create server
+    app.listen(port,()=>{
+        console.log("Server running correctly at the url:localhost:3700");
+    });
+})
+.catch(err =>console.log(err));
