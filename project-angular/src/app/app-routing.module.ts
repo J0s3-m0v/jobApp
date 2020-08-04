f2fd5cb621e53b33e919e6a53b2203627c851ca3
J0s3-m0v <j0s3.m0v@outlook.es>
2020-08-04 21:45:31 +0100

    create routing

--- project-angular/src/app/app-routing.module.ts
+++ project-angular/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { CreateComponent } from './components/create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'jobs',
+    component:JobsComponent
+  },  
+  {
+    path:'create',
+    component:CreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project-angular/src/app/app.module.ts
+++ project-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { CreateComponent } from './components/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JobsComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
